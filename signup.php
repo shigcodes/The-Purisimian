23b879886515565e3576047b4e601832b721d43f
shigcodes <shigridanndocil11@gmail.com>
2023-11-18 21:21:41 +0800

    Initial commit

--- signup.php
+++ signup.php
@@ -39,36 +39,32 @@ if (isset($_POST['register'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <!-- bootstrap css link -->
+   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- font awesome link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
         integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- stylesheet -->
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <div class="container d-flex align-items-center justify-content-center">
         <div class="card">
-            <!-- card header -->
+          
             <div class="card-header">
                 <h3 class="text-center">Registration Form</h3>
             </div>
-            <!-- card body -->
+         
             <div class="card-body">
                 <form action="" method="post">
-                    <!-- first field -->
+                  
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-user"></i></span>
                         <input type="text" class="form-control" placeholder="Enter your username" required="required"
                             autocomplete="off" name="username" aria-label="Username" aria-describedby="basic-addon1">
                     </div>
-                    <!-- second field -->
+                    
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1"><i class="fas fa-key"></i></span>
                         <input type="password" class="form-control" placeholder="Enter your password"
